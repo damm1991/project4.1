2ae0457a8b98ff4c8a78f1e6812f7e9ef52b9c5d
damm1991 <robert_damminga@hotmail.com>
2018-11-15 18:03:44 +0100

    added clientscript

---
+++ clientscript/clientscript.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+echo ========================================INSTALLING SALTSTACK=============================================
+wget -O - https://repo.saltstack.com/apt/ubuntu/16.04/amd64/latest/SALTSTACK-GPG-KEY.pub | apt-key add -
+echo "deb http://repo.saltstack.com/apt/ubuntu/16.04/amd64/latest xenial main" > /etc/apt/sources.list.d/saltstack.list
+sudo apt-get update
+sudo apt-get install salt-minion -y
+echo ========================================CONFIGURING SALTSTACK============================================
+sed -i '/#master:/ a master: 192.168.0.20' /etc/salt/minion
+service salt-minion restart
