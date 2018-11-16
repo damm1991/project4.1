0e3d7c6206948bbd6e25f466a2ed9f387fa16e29
damm1991 <robert_damminga@hotmail.com>
2018-11-16 01:57:22 +0100

    added all

--- clientscript.sh
+++ clientscript.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
+echo ========================================INSTALLING NFS CLIENT============================================
+sudo apt-get update
+sudo apt-get install rpcbind nfs-common -y
 echo ========================================INSTALLING SALTSTACK=============================================
 wget -O - https://repo.saltstack.com/apt/ubuntu/16.04/amd64/latest/SALTSTACK-GPG-KEY.pub | apt-key add -
 echo "deb http://repo.saltstack.com/apt/ubuntu/16.04/amd64/latest xenial main" > /etc/apt/sources.list.d/saltstack.list
